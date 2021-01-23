d2aa4e68ffa5d085009fa1fc052e2c68f8926598
Mourad <mourad.44@hotmail.com>
2021-01-24 00:17:55 +0200

    URP, movement test, light test, tiles

---
+++ The Echo of Light/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] Rigidbody2D rb2d;
+    [SerializeField] int speed=5;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb2d.velocity=new Vector2(Input.GetAxisRaw("Horizontal")*speed,0);
+    }
+}
