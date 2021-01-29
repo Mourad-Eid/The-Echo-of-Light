f68d2dc1678fdde9b1828a4f6e985b95a1c799cc
Caroline Ehab <68237645+CarolineEhabAnwar@users.noreply.github.com>
2021-01-29 22:53:50 +0200

    Some Adjustments

--- The Echo of Light/Assets/Scripts/SceneManager.cs
+++ The Echo of Light/Assets/Scripts/SceneManager.cs
@@ -2,18 +2,26 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class SceneManager : MonoBehaviour
 {
+    //public Animator transition;
+    //public float transitionTime = 1f;
     public void LoadScene(string sceneName)
     {
+        //StartCoroutine(LoadWantedScene(sceneName));
         UnityEngine.SceneManagement.SceneManager.LoadScene(sceneName);
+
+
     }
 
     public void LoadMainMenu()
     {
         Time.timeScale = 1;
+        //StartCoroutine(LoadWantedScene("Main Menu"));
         UnityEngine.SceneManagement.SceneManager.LoadScene("Main Menu");
+
     }
 
     public void CloseGame()
@@ -21,4 +29,10 @@ public class SceneManager : MonoBehaviour
         Application.Quit();
     }
 
+    //IEnumerator LoadWantedScene(string sceneName)
+    //{
+    //    transition.SetTrigger("Start");
+    //    yield return new WaitForSeconds(transitionTime);
+    //    UnityEngine.SceneManagement.SceneManager.LoadScene(sceneName);
+    //}
 }
